54c5f852af263fd06bdc48f1e998033fd13f9c03
Finch Yuan <fyuan@chicagobooth.edu>
2023-04-07 23:41:04 +0000

    Rock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    # code of play_rock
+
+    # redirect_to("https://www.google.com")
+
+    # render({:plain=>"heeeel"})
+
+    # render({:plain=>rand(100)})
+
+    # render({:html => "<h1>Hello, World!<h1>".html_safe}) 
+
+    render({ :template => "game_templates/user_rock.html.erb" })
+
+  end
 end
